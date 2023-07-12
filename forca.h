549a88efeba8ccecd466a013d229942bc3ae9f19
Gilmar Correa Morais Junior <127146212+Arqgilmar@users.noreply.github.com>
2023-07-12 13:41:46 -0300

    Create forca.h

---
+++ forca.h
@@ -0,0 +1,9 @@
+void abertura();
+void chuta();
+void desenhaforca();
+void escolhepalavra();
+void adicionapalavra();
+int acertou();
+int enforcou();
+int jachutou(char letra);
+int chuteserrados();
